d7ee0eabe5388b3e4c56bced4d8e37d66838a319
yonash24 <yonashay87@gmail.com>
2025-08-24 14:35:09 +0300

    replace list to dictionary and add data clean func

--- dataHandler.py
+++ dataHandler.py
@@ -2,87 +2,99 @@ import pandas as pd
 import numpy as np
 import matplotlib as plt
 import os
+from sklearn.preprocessing import MinMaxScaler
 from kaggle.api.kaggle_api_extended import KaggleApi
 
 #class that dealing with importing the datasets
 class ImportData:
     
     #import the data from kaggles 
-    #return list of dataFrames of the files 
+    #return dictionary of dataFrames of the files 
     @staticmethod
     def import_data():
         api = KaggleApi()
         api.authenticate()
         api.dataset_download_files("garymk/movielens-25m-dataset",path='.',unzip=True)
-        datafram_list = []
+        datafram_dict = {}
         file_path = "ml-25m"
         for file in os.listdir(file_path):
             if file.endswith(".csv"):
                 df = pd.read_csv(os.path.join(file_path,file))
-                datafram_list.append(df)
-        return datafram_list
+                datafram_dict[file_path] = df
+        return datafram_dict
     
 #class that handel the data cleaning
-#get a list of dataFrame and clean the data
+#get a dictionary of dataFrame and clean the data
 #all the functions get the dataframe with the files from import_data
 class DataCleaning:
 
     #check for missing data
     @staticmethod
-    def data_check(dataset_list):
-        cleaned_dataframes = []
-        for data in dataset_list:
+    def data_check(dataset_dict:dict):
+        cleaned_dataframes = {}
+        for data in dataset_dict:
             new_data = data.dropna()
-            cleaned_dataframes.append(new_data)
+            cleaned_dataframes[data] = new_data
         return cleaned_dataframes
     
-    #dealing with duplicates
+    #remove duplicates
     @staticmethod
-    def remove_dup(dataset_list):
-        new_data = []
+    def remove_dup(dataset_list:dict):
+        new_data = {}
         for file in dataset_list:
             undup_data = file.drop_duplicates()
-            new_data.append(undup_data)
+            new_data[file] = undup_data
         return new_data
-    
-    
     
     
     
     #fitting the data types
     @staticmethod
-    def data_fit(dataset_list):
-        new_data = []
-        rating_dict = {"userId":"int64", "movieId":"int64", "rating":"float64", "timestamp":"int64"}
-        tag_dict = {"userId":"int64", "movieId":"int64", "tag":"object", "timestamp":"int64"}
-        moovies_dict = {"movieId":"int64", "title":"object", "genres":"object"}
-        link_dict = {"movieId":"int64", "imdbId":"int64", "tmdbId":"int64"}
-        genome_score_dict = {"movieId":"int64", "tagId":"int64", "relevance":"float64"}
-        genome_tag_dict = {"tagId":"int64", "tag":"object"}
+    def data_fit(dataset_dict:dict):
+        new_data = {}
+        type_map ={ "ratings.csv":{"userId":"int64", "movieId":"int64", "rating":"float64", "timestamp":"int64"},
+                    "tags.csv": {"userId":"int64", "movieId":"int64", "tag":"object", "timestamp":"int64"},
+                    "movies.csv": {"movieId":"int64", "title":"object", "genres":"object"},
+                    "links.csv" :{"movieId":"int64", "imdbId":"int64", "tmdbId":"int64"},
+                    "genome-scores.csv": {"movieId":"int64", "tagId":"int64", "relevance":"float64"},
+                    "genome-tags.csv": {"tagId":"int64", "tag":"object"}}
         
-        for data in dataset_list:
-                column_header = data.columns
-                col_list = list(column_header)            
-                if col_list == ["tagId", "tag"]:
-                    new_genome_tags_data = data.astype(genome_tag_dict)
-                    new_data.append(new_genome_tags_data)
-                elif col_list == ["movieId", "tagId", "relevance"]:
-                    new_genome_scores_data = data.astype(genome_score_dict)
-                    new_data.append(new_genome_scores_data)
-                elif col_list == ["movieId", "imdbId", "tmdbId"]:
-                    new_links_data = data.astype(link_dict)
-                    new_data.append(new_links_data)
-                elif col_list == ["movieId", "title", "genres"]:
-                    new_movies_data = data.astype(moovies_dict)
-                    new_data.append(new_movies_data)
-                elif col_list == ["userId", "movieId", "tag", "timestamp"]:
-                    new_tags_data = data.astype(tag_dict)
-                    new_data.append(new_tags_data)
-                elif col_list == ["userId", "movieId", "rating", "timestamp"]:
-                    new_ratings_data = data.astype(rating_dict)
-                    new_data.append(new_ratings_data)
+        for file_name, data in dataset_dict.items():
+            if file_name in type_map:
+                cur_type = type_map[file_name]
+                new_data[file_name] = data.astype(cur_type)
+            else:
+                new_data[file_name] = data
                 
         return new_data
     
     
+    #normalize the data
+    @staticmethod
+    def data_normalaize(dataset_dict: dict):
+        scaler = MinMaxScaler()
+        
+        for file_name in dataset_dict:
+            df = dataset_dict[file_name]
+            for data in df.columns:
+                if pd.api.types.is_numeric_dtype(df[data]):
+                    df["normalize_"+data] = scaler.fit_transform(df[[data]])
+                    
+        return dataset_dict
     
+    
+    #categorical the data
+    #go through the dataframes and seperate the text cols for each element 
+    #give 1 if the element within the col 0 otherwize
+    @staticmethod
+    def categorical_data(data_dict:dict):
+        new_data_dict = {}
+        for file_name, df in data_dict.items():
+            text_cols = df.select_dtypes(include=["object"]).columns
+            if not text_cols.empty:
+                dummies = pd.get_dummies(df[text_cols])
+                df = df.drop(columns=text_cols)
+                df = pd.concat([df,dummies],axis=1)
+            new_data_dict[file_name] = df
+            
+        return new_data_dict
